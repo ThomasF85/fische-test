535a1d1c47f48610c94ba7d07e1c5aa8d09f54f6
ThomasF85 <thomas.foeldi@gmail.com>
2022-10-02 09:32:01 +0200

    add mongoose

--- services/productService.js
+++ services/productService.js
@@ -1,66 +1,26 @@
-const products = [
-  {
-    id: "1",
-    name: "Garnele",
-    description:
-      "Lebt bevorzugt paarweise. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quaerat ea sint ipsum nisi recusandae corrupti ab possimus eveniet voluptate eaque ullam quas voluptatem omnis quod, natus, non eum assumenda.",
-    price: 19,
-    category: "Wirbellose",
-  },
-  {
-    id: "2",
-    name: "Anemonenfisch",
-    description:
-      "Nemo. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quaerat ea sint ipsum nisi recusandae corrupti ab possimus eveniet voluptate eaque ullam quas voluptatem omnis quod, natus, non eum assumenda.",
-    price: 60,
-    category: "Meerwasser",
-  },
-  {
-    id: "3",
-    name: "Pracht-Anemone",
-    description:
-      "Ist prächtig. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quaerat ea sint ipsum nisi recusandae corrupti ab possimus eveniet voluptate eaque ullam quas voluptatem omnis quod, natus, non eum assumenda.",
-    price: 90,
-    category: "Korallen und Blumentiere",
-  },
-  {
-    id: "4",
-    name: "Mördermuschel",
-    description:
-      "Ist hübscher als ihr Name. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quaerat ea sint ipsum nisi recusandae corrupti ab possimus eveniet voluptate eaque ullam quas voluptatem omnis quod, natus, non eum assumenda.",
-    price: 125,
-    category: "Muscheln",
-  },
-  {
-    id: "5",
-    name: "Kaiserfisch",
-    description:
-      "Ein tagaktiver Einzelgänger. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quaerat ea sint ipsum nisi recusandae corrupti ab possimus eveniet voluptate eaque ullam quas voluptatem omnis quod, natus, non eum assumenda.",
-    category: "Meerwasser",
-    price: 90,
-  },
-  {
-    id: "6",
-    name: "Guppy",
-    description:
-      "Klein aber fein. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quaerat ea sint ipsum nisi recusandae corrupti ab possimus eveniet voluptate eaque ullam quas voluptatem omnis quod, natus, non eum assumenda.",
-    price: 5,
-    category: "Süßwasser",
-  },
-  {
-    id: "7",
-    name: "Regenbogenfisch",
-    description:
-      "In allen Farben und bunt. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quaerat ea sint ipsum nisi recusandae corrupti ab possimus eveniet voluptate eaque ullam quas voluptatem omnis quod, natus, non eum assumenda.",
-    price: 12,
-    category: "Süßwasser",
-  },
-];
+import dbConnect from "../lib/dbConnect";
+import Product from "../models/Product";
 
 export async function getAllProducts() {
-  return products;
+  await dbConnect();
+
+  const products = await Product.find().populate("category");
+
+  return products.map(({ id, name, description, price, category }) => ({
+    id,
+    name,
+    description,
+    price,
+    category: category.name,
+  }));
 }
 
-export async function getProductById(id) {
-  return products.find((product) => product.id === id);
+export async function getProductById(categoryId) {
+  await dbConnect();
+
+  const product = await Product.findById(categoryId).populate("category");
+
+  const { id, name, description, price, category } = product;
+
+  return { id, name, description, price, category: category.name };
 }
